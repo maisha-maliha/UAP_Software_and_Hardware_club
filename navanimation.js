58617ce85c61f0de16e85b998ca220b28e495ee6
Maisha <officiallymaisha@gmail.com>
2021-06-26 20:21:38 +0600

    nav almost done

--- navanimation.js
+++ navanimation.js
@@ -1,13 +1,19 @@
 var burger =document.getElementsByClassName('burger')[0];
-var nav_cover =document.getElementsByClassName('nav-cover')[0];
+var nav_cover = document.getElementsByClassName('nav-cover')[0];
+var nav = document.querySelector("nav");
 var active = true;
+
+
 burger.onclick = function(){
     console.log(nav_cover);
     if(active){
         nav_cover.style.animationName = "show";
+        nav.className = 'navon';
+        console.log(nav);
         active = false;
     } else{
         nav_cover.style.animationName = "hide";
+        nav.className = '';
         active = true;
     }
 };
