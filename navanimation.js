f3a9b9c65efc0fada1945df9f4550ef9b90a5d0a
Maisha <officiallymaisha@gmail.com>
2021-06-26 18:08:55 +0600

    hi

--- navanimation.js
+++ navanimation.js
@@ -1,5 +1,12 @@
 var burger =document.getElementsByClassName('burger')[0];
-
+var nav_cover =document.getElementsByClassName('nav-cover')[0];
+var active = true;
 burger.onclick = function(){
-    console.log("hello");
+    console.log(nav_cover);
+    if(active){
+        nav_cover.style.animationName = "show";
+        active = false;
+    } else{
+        nav_cover.style.animationName = "hide";
+    }
 };
