7b97acbecccfcebafbe2234b5848640007a25f13
Maisha <officiallymaisha@gmail.com>
2021-06-30 20:40:05 +0600

    images slider DONE

--- resources/js/navanimation.js
+++ resources/js/navanimation.js
@@ -1,17 +1,18 @@
 var burger = document.getElementsByClassName('burger')[0];
 var nav_cover = document.getElementsByClassName('nav-cover')[0];
+var contact = document.querySelector('nav ul li:nth-of-type(4)');
 var nav = document.querySelector("nav");
 var active = true;
-console.log(nav_cover);
-
+var element = document.getElementsByTagName('footer')[0];
+var w = window.innerWidth;
 burger.addEventListener("click", navopen);
+contact.addEventListener("click", conscroll);
 
 function navopen(){
-    console.log(nav_cover);
     if(active){
         nav_cover.style.animationName = "show";
         nav.className = 'navon';
-        console.log(nav);
+        console.log(contact);
         active = false;
     } else{
         nav_cover.style.animationName = "hide";
@@ -19,3 +20,10 @@ function navopen(){
         active = true;
     }
 };
+function conscroll(){
+    console.log(w)
+    if(w < 1100){
+        navopen();
+    }
+    element.scrollIntoView();
+}
